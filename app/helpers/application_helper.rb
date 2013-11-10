a492a4200799f3f96fab70d9afa08fb5dd38a05c
Artem Aminov <artemaminov@gmail.com>
2013-11-10 19:02:20 +0600

    Add Nav pointer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def nav_link(name, path, options = {})
+		current = current_page?(path) ? 'current' : nil
+		@content = link_to(name, path, options)
+		@content << content_tag(:div, '', class: 'pointer') if current
+		content_tag :li, @content
+	end
+
+
 end
